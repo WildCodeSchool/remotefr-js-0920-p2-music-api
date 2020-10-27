d28c342bebfad9b9582c918459e062c8859a5873
lgiacalo <lgiacalo@student.42.fr>
2020-10-27 16:24:56 +0100

    Refactor App with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { AuthToken, AuthTokens, ServiceName, TokenContext } from './TokenContext';
 import LoginButton from './components/LoginButton';
 
-class App extends React.Component<unknown, { tokens: AuthTokens }> {
-  constructor(props) {
-    super(props);
-
-    // Load from localstorage or default null values
-    const defaultServices: Record<ServiceName, AuthToken> = JSON.parse(String(localStorage.getItem('services'))) ?? {
+function getServicesLocalStorage(): Record<ServiceName, AuthToken> {
+  return (
+    JSON.parse(String(localStorage.getItem('services'))) ?? {
       spotify: {
         token: null,
         expirationStamp: -1,
@@ -17,52 +14,63 @@ class App extends React.Component<unknown, { tokens: AuthTokens }> {
         token: null,
         expirationStamp: -1,
       },
-    };
+    }
+  );
+}
 
-    this.state = {
-      tokens: {
-        services: defaultServices,
-        setToken: (name, value, expiresIn): void => {
-          const { tokens } = this.state;
-          tokens.services[name].token = value;
-          // expiresIn is in seconds, so we convert it to miliseconds before adding it to the current timestamp
-          tokens.services[name].expirationStamp = Date.now() + expiresIn * 1000;
+// Custom hook to handle services (service / setToken / removeToken)
+function useServices(
+  initServices: Record<ServiceName, AuthToken>
+): [
+  Record<ServiceName, AuthToken>,
+  (name: ServiceName, value: string, expiresIn: number) => void,
+  (name: ServiceName) => void
+] {
+  const [services, setService] = useState(initServices);
 
-          localStorage.setItem('services', JSON.stringify(tokens.services));
-          this.setState({ tokens });
-        },
-        removeToken: (name): void => {
-          const { tokens } = this.state;
-          tokens.services[name].token = null;
-          tokens.services[name].expirationStamp = -1;
+  const setToken = (name: ServiceName, value: string, expiresIn: number): void => {
+    services[name].token = value;
+    // expiresIn is in seconds, so we convert it to miliseconds before adding it to the current timestamp
+    services[name].expirationStamp = Date.now() + expiresIn * 1000;
 
-          localStorage.setItem('services', JSON.stringify(tokens.services));
-          this.setState({ tokens });
-        },
-      },
-    };
+    localStorage.setItem('services', JSON.stringify(services));
+    setService({ ...services });
+  };
 
-    // Remove expired tokens
-    const { tokens } = this.state;
-    Object.entries(tokens.services).forEach(([name, service]): void => {
+  const removeToken = (name: ServiceName): void => {
+    services[name].token = null;
+    services[name].expirationStamp = -1;
+
+    localStorage.setItem('services', JSON.stringify(services));
+    setService({ ...services });
+  };
+
+  return [services, setToken, removeToken];
+}
+
+// export default App;
+const App = (): JSX.Element => {
+  const [services, setToken, removeToken] = useServices(getServicesLocalStorage());
+
+  const value: AuthTokens = { services, setToken, removeToken };
+
+  // Verification timeStamp
+  useEffect(() => {
+    Object.entries(services).forEach(([name, service]): void => {
       if (service.expirationStamp !== -1 && Date.now() > service.expirationStamp) {
-        tokens.removeToken(name as ServiceName);
+        removeToken(name as ServiceName);
       }
     });
-  }
+  }, []);
 
-  render(): JSX.Element {
-    const { tokens } = this.state;
-
-    return (
-      <TokenContext.Provider value={tokens}>
-        <div className="App">
-          <LoginButton service="spotify" />
-          <LoginButton service="youtube" />
-        </div>
-      </TokenContext.Provider>
-    );
-  }
-}
+  return (
+    <TokenContext.Provider value={value}>
+      <div className="App">
+        <LoginButton service="spotify" />
+        <LoginButton service="youtube" />
+      </div>
+    </TokenContext.Provider>
+  );
+};
 
 export default App;
